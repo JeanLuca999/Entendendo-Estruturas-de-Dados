0965c9099af3453ae0d4078bde0cb1a38877ed67
JeanLuca999 <66375859+JeanLuca999@users.noreply.github.com>
2020-10-15 16:31:36 -0300

    Funções da pilha quase prontas

--- js/script.js
+++ js/script.js
@@ -11,15 +11,27 @@ function Pilha(){
     container.style.flexDirection = 'column'
     container.style.width = '100px'
     container.style.margin = '0 auto'
-    container.setAttribute('class', 'container')
     for(let i = 0; i < 3; i++){ //criando cada espaço interno da pilha
         let elemento = document.createElement('div')
         elemento.style.width = '100px'
         elemento.style.height = '80px'
         elemento.style.border = '1px solid black'
+        elemento.style.backgroundColor = 'white'
+        elemento.setAttribute('class', 'container')
         container.appendChild(elemento)
     }
     let pilha = document.getElementById('container-da-pilha') //adicionando a pilha ao corpo do site
     pilha.appendChild(container)
     document.getElementById('pilha').style.display = 'block'
+}
+
+function inserirPilha(){
+    for(let i = 2; i >= 0; i--){ //o container de indice 2 eh o container da base, por isso começamos por ele
+        if( document.getElementsByClassName('container')[i].style.backgroundColor == 'white'){
+            document.getElementsByClassName('container')[i].style.backgroundColor = 'lightgrey'
+            document.getElementsByClassName('container')[i].innerHTML = `${Math.abs(i-2)}`
+            return
+        }
+    }
+    window.alert('pilha cheia')     
 }
